8d304c29e1bb1bda1ddd962c8dbc4698efeef8eb
horngperng <horngperng@gmail.comm>
2019-12-18 22:31:10 +0800

    first commit

---
+++ faker.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Faker = require('faker');
+const uuidv1 = require('uuid/v1');
+// https://artillery.io/blog/using-fakerjs-with-artillery
+
+module.exports = {
+    fakeIt
+};
+
+
+function fakeIt(userContext, events, done) {
+
+    const id = uuidv1()
+    const sender = {
+        id: uuidv1(),
+        session_id: uuidv1(),
+        appSource: null,
+        role: null
+    }
+    const recipient = {
+        id: 'ENDPOINT'
+    }
+    const postback = {
+        event: "handover_livechat_start",
+        data: true,
+        text: "Proceed",
+        isPreview: false
+    }
+
+    // generate data with Faker:
+    // const name = `${Faker.name.firstName()} ${Faker.name.lastName()}`;
+    // const email = Faker.internet.exampleEmail();
+    // const password = Faker.internet.password();
+    // add variables to virtual user's context:
+    userContext.vars.id = id;
+    userContext.vars.sender = sender;
+    userContext.vars.recipient = recipient;
+    userContext.vars.postback = postback;
+    return done();
+}
